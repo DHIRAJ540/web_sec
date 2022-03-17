ae105fdc2b35d40c577805fe0a060ed7569c917e
DHIRAJ540 <dhirajsubudhi540@gmail.com>
2022-03-18 00:35:36 +0530

    08032022

--- src/components/Icon/styles.js
+++ src/components/Icon/styles.js
@@ -1,56 +1,9 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  width: 28%;
-  
-  color: #005288;
-  padding:.8rem;
-  background-color: #fff;
-  margin-left: 2%;
-  margin-right: 2%;
-  border-radius: 10px;
-  margin-bottom: 2.5%;
-  display:flex;
-  align-items:center;
-  cursor: default;
-  &:hover {
-    position: relative;
-    outline: 2px solid #19b0f7;
-  }
-  @media(max-width:1200px){
-    width: 45%
+export const Container = styled.div` `;
 
-  }
-`;
-// height: 117px;
-// width: 96px;
-// display: flex;
-// cursor: pointer;
-// flex-direction: column;
-// justify-content: space-between;
-// align-items: center;
-// font-family: Lato, sans-serif;
-// font-weight: lighter;
-// color: #535b62;
-// margin-bottom: 22px;
-// padding: 8px 7px 10px 10px;
-// border-radius: 8px;
-// transition: background 230ms ease-in;
-// &:hover {
-//   background: #e6f5ff;
-// }
+export const Logo = styled.div` `;
 
-export const Logo = styled.div`
-  
-`;
+export const Img = styled.img` `;
 
-export const Img = styled.img`
-  
-`;
-
-export const Name = styled.div`
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  overflow: hidden;
-  margin-left:1rem
-`;
+export const Name = styled.div` `;
